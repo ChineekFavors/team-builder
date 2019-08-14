7c4eb9969b64b66a03b55cbe056a4c915065c20f
Chineek Favors <chineekfavors01@gmail.com>
2019-08-13 18:46:22 -0700

    created Heading component

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Heading(){
+
+	return(
+		<h1 className='heading'>Team Builder</h1>
+	)
+
+}
+
+
+export default Heading;
---
+++ src/components/heading/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Heading(){
+
+	return(
+		<h1>Team Builder</h1>
+	)
+
+}
+
+
+export default Heading;

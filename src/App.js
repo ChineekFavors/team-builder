3c73f54d189b2a611673772fd5858c8089173569
Chineek Favors <chineekfavors01@gmail.com>
2019-08-13 19:09:30 -0700

    created a data.js file

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import './App.css';
 
+import data from './data.js'
+
 import Heading from './components/heading/Heading.js'
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
     <Heading />
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export default [
+	{
+		name: 'chineek',
+		email: 'chineekfavors01@gmail.com',
+		role: 'full stack web developer'	
+	},
+	{
+		name: 'jessica',
+		email: 'jessica27@gmail.com',
+		role: 'part-time student'	
+	},
+
+];
